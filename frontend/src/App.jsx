7cf539100c098e90e696838e8f95d814ad33f714
Renato <renato.f.fabri@gmail.com>
2023-11-27 17:28:11 -0300

    Add initial routes and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { Container } from "@chakra-ui/react";
+import UserPage from "./pages/UserPage";
+import PostPage from "./pages/PostPage";
+
 function App() {
   return (
     <Container maxWidth="620px">
       <Routes>
-        <Route path="/:username" />
+        <Route path="/:username" element={UserPage} />
+        <Route path="/:username/post/:pid" element={PostPage} />
       </Routes>
     </Container>
   );
