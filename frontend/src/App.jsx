ed338fafd568259b9501b9bf6fccbc31c3e530a4
Renato <renato.f.fabri@gmail.com>
2023-11-27 17:43:46 -0300

    Add Header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { Container } from "@chakra-ui/react";
+import Header from "./components/Header";
 import UserPage from "./pages/UserPage";
 import PostPage from "./pages/PostPage";
 
 function App() {
   return (
     <Container maxWidth="620px">
+      <Header />
       <Routes>
         <Route path="/:username" element={UserPage} />
         <Route path="/:username/post/:pid" element={PostPage} />
