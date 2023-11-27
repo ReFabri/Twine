301b35bd38a3f564638b4d4d25916919e2be481b
Renato <renato.f.fabri@gmail.com>
2023-11-27 15:18:43 -0300

    Add and config Chakra UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div>App</div>;
+  return <>App</>;
 }
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ChakraProvider, extendTheme, ColorModeScript } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: true,
+};
+
+const styles = {
+  global: (props) => ({
+    body: {
+      color: mode("gray.800", "whiteAlpha.900")(props),
+      bg: mode("gray.100", "#101010")(props),
+    },
+  }),
+};
+
+const colors = {
+  gray: {
+    light: "#616161",
+    dark: "#1e1e1e",
+  },
+};
+
+const theme = extendTheme({ config, styles, colors });
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
