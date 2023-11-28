8fda3add13cb3ec4e719e55e5ccefa161e97f543
Renato <renato.f.fabri@gmail.com>
2023-11-28 17:45:14 -0300

    Improved UserHeader html and style

--- frontend/src/components/UserHeader.jsx
+++ frontend/src/components/UserHeader.jsx
@@ -1,6 +1,25 @@
-import { Avatar, Box, Flex, VStack, Text } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Flex,
+  VStack,
+  Text,
+  Link,
+  Menu,
+  MenuButton,
+  Portal,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
+import { BsInstagram } from "react-icons/bs";
+import { CgMoreO } from "react-icons/cg";
 
 const UserHeader = () => {
+  const copyURL = () => {
+    const currentURL = window.location.href;
+    navigator.clipboard.writeText(currentURL);
+  };
+
   return (
     <VStack gap={4} alignItems={"start"}>
       <Flex justifyContent={"space-between"} width={"full"}>
@@ -17,13 +36,43 @@ const UserHeader = () => {
               p={1}
               borderRadius={"full"}
             >
-              twine.next
+              twine.net
             </Text>
           </Flex>
         </Box>
         <Box>
           <Avatar name="Dude Realperson" src="/avatar.jpg" size={"xl"} />
         </Box>
+      </Flex>
+      <Text>
+        Executive chairman and CEO of a real monolith company that really exists
+        for real.
+      </Text>
+      <Flex w={"full"} justifyContent={"space-between"}>
+        <Flex gap={2} alignItems={"center"}>
+          <Text color={"gray.light"}>3.2K followers</Text>
+          <Box w={1} h={1} bg={"gray.light"} borderRadius={"full"}></Box>
+          <Link color={"gray.light"}>instagram.com</Link>
+        </Flex>
+        <Flex>
+          <Box className="icon-container">
+            <BsInstagram size={24} cursor={"pointer"} />
+          </Box>
+          <Box className="icon-container">
+            <Menu>
+              <MenuButton>
+                <CgMoreO size={24} cursor={"pointer"} />
+              </MenuButton>
+              <Portal>
+                <MenuList bg={"gray.dark"}>
+                  <MenuItem bg={"gray.dark"} onClick={copyURL}>
+                    Copy link
+                  </MenuItem>
+                </MenuList>
+              </Portal>
+            </Menu>
+          </Box>
+        </Flex>
       </Flex>
     </VStack>
   );
