4731f6a70a21bd1950dbfbe353f160554527ab29
Renato <renato.f.fabri@gmail.com>
2023-11-30 20:05:47 -0300

    Add UserPost content and style

--- frontend/src/components/UserPost.jsx
+++ frontend/src/components/UserPost.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-import { Flex, Avatar, Box } from "@chakra-ui/react";
-
+import { Flex, Avatar, Box, Text, Image } from "@chakra-ui/react";
+import { BsThreeDots } from "react-icons/bs";
 const UserPost = () => {
   return (
     <Link to={"/dude/post/1"}>
@@ -8,7 +8,51 @@ const UserPost = () => {
         <Flex flexDirection={"column"} alignItems={"center"}>
           <Avatar size={"md"} name="Dude RealPerson" src="/avatar.jpg" />
           <Box w={"1px"} h={"full"} bg={"gray.light"} my={2}></Box>
-          <Box position={"relative"} w={"full"}></Box>
+          <Box position={"relative"} w={"full"}>
+            <Avatar
+              size={"xs"}
+              name="John Doe"
+              src="https://bit.ly/dan-abramov"
+              position={"absolute"}
+              top={"0px"}
+              left={"15px"}
+              padding={"2px"}
+            />
+            <Avatar
+              size={"xs"}
+              name="John Doe"
+              src="https://bit.ly/ryan-florence"
+              position={"absolute"}
+              bottom={"0px"}
+              right={"-5px"}
+              padding={"2px"}
+            />
+            <Avatar
+              size={"xs"}
+              name="John Doe"
+              src="https://bit.ly/kent-c-dodds"
+              position={"absolute"}
+              bottom={"0px"}
+              left={"4px"}
+              padding={"2px"}
+            />
+          </Box>
+        </Flex>
+        <Flex flex={1} flexDirection={"column"} gap={2}>
+          <Flex justifyContent={"space-between"} w={"full"}>
+            <Flex w={"full"} alignItems={"center"}>
+              <Text fontSize={"sm"} fontWeight={"bold"}>
+                duderealperson
+              </Text>
+              <Image src="/verified.png" w={4} h={4} ml={1} />
+            </Flex>
+            <Flex gap={4} alignItems={"center"}>
+              <Text fontSize={"sm"} color={"gray.light"}>
+                1d
+              </Text>
+              <BsThreeDots />
+            </Flex>
+          </Flex>
         </Flex>
       </Flex>
     </Link>
