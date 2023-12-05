15891abe53cf98ac209ae8dc8a8b704231718d32
Renato <renato.f.fabri@gmail.com>
2023-12-04 21:05:16 -0300

    Add userRoutes to server.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,12 @@
 import express from "express";
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.status(200).json({ message: "ALL IS WELL !" });
+});
+
+router.post("/signup", (req, res) => {
+  res.send("Signed up successfully");
+});
+
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 const app = express();
