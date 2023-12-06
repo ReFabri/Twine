064de13206d832449b2fa067f2d3da0703df0bc1
Renato <renato.f.fabri@gmail.com>
2023-12-06 19:50:42 -0300

    Fix user validation to protectRoute middleware

--- backend/middlewares/protectRoute.js
+++ backend/middlewares/protectRoute.js
@@ -8,6 +8,7 @@ const protectRoute = async (req, res, next) => {
     if (!token) return res.status(401).json({ message: "Unauthorized" });
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded.userId).select("-password");
+    if (!user) return res.status(401).json({ message: "Unauthorized" });
     req.user = user;
     next();
   } catch (error) {
