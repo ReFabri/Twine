59f9d4e251ed7b3a4658e5a49c4d5a94eb648cb4
Renato <renato.f.fabri@gmail.com>
2023-12-08 15:43:58 -0300

    Add createPost initial route controller

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,8 +1,26 @@
+import Post from "../models/postModel.js";
+import User from "../models/userModel.js";
 import errorHandler from "../utils/helpers/errorHandler.js";
 
 export const createPost = async (req, res) => {
   try {
-    res.send("ROUTE TODO");
+    const { postedBy, text } = req.body;
+    let { img } = req.body;
+
+    if (!postedBy || !text)
+      return res.status(400).json({ message: "Unable to create Post" });
+
+    const user = await User.findById(postedBy);
+    if (!user) return res.status(401).json({ message: "Unauthorized" });
+
+    if (String(user._id) !== String(req.user._id))
+      return res.status(401).json({ message: "Unauthorized" });
+
+    const maxLength = 500;
+    if (text.length < maxLength)
+      return res
+        .status(400)
+        .json({ message: `Post exceeds ${maxLength} characters` });
   } catch (error) {
     errorHandler(error, res);
   }
