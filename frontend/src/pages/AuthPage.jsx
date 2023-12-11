fcc44839041b8abd5f267281edb8cc4b7ec3d9bf
Renato <renato.f.fabri@gmail.com>
2023-12-11 19:46:26 -0300

    Fix to authPage state management

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,11 +1,10 @@
 import SignupCard from "../components/SignupCard";
 import LoginCard from "../components/LoginCard";
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useRecoilValue } from "recoil";
 import authScreenAtom from "../atoms/authAtom";
 
 const AuthPage = () => {
   const authScreenState = useRecoilValue(authScreenAtom);
-  useSetRecoilState(authScreenAtom);
   return <>{authScreenState === "login" ? <LoginCard /> : <SignupCard />}</>;
 };
 
