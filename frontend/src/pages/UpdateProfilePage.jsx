8edfa6b1dbb41ca2c0e9666e0578ead5a653222c
Renato <renato.f.fabri@gmail.com>
2023-12-13 17:59:36 -0300

    Fix updateProfilePage react conflict among controlled components

--- frontend/src/pages/UpdateProfilePage.jsx
+++ frontend/src/pages/UpdateProfilePage.jsx
@@ -3,6 +3,7 @@ import usePreviewImg from "../hooks/usePreviewImg";
 import useShowToast from "../hooks/useShowToast";
 import { useRef, useState } from "react";
 import { useRecoilState } from "recoil";
+import { BiSolidShow, BiSolidHide } from "react-icons/bi";
 import {
   Button,
   Flex,
@@ -15,17 +16,20 @@ import {
   Avatar,
   Center,
   Textarea,
+  InputGroup,
+  InputRightElement,
 } from "@chakra-ui/react";
 
 const UpdateProfilePage = () => {
   const [user, setUser] = useRecoilState(userAtom);
-  const [inputs, setInputs] = useState({
-    name: user.name,
-    username: user.username,
-    email: user.email,
-    bio: user.bio,
+  const [showPassword, setShowPassword] = useState(false);
+  const [inputs, setInputs] = useState(() => ({
+    name: user.name || "",
+    username: user.username || "",
+    email: user.email || "",
+    bio: user.bio || "",
     password: "",
-  });
+  }));
   const fileRef = useRef(null);
   const [updating, setUpdating] = useState(false);
 
@@ -52,7 +56,7 @@ const UpdateProfilePage = () => {
       }
       showToast("Success", "Profile updated successfully", "success");
       setUser(data);
-      localStorage.setItem("user-threads", JSON.stringify(data));
+      localStorage.setItem("user-twine", JSON.stringify(data));
     } catch (error) {
       showToast("Error", error, "error");
     } finally {
@@ -101,7 +105,9 @@ const UpdateProfilePage = () => {
             <Input
               placeholder={user.name}
               value={inputs.name}
-              onChange={(e) => setInputs({ ...inputs, name: e.target.value })}
+              onChange={(e) =>
+                setInputs((prev) => ({ ...prev, name: e.target.value }))
+              }
               _placeholder={{ color: "gray.500" }}
               type="text"
             />
@@ -112,7 +118,7 @@ const UpdateProfilePage = () => {
               placeholder={user.username}
               value={inputs.username}
               onChange={(e) =>
-                setInputs({ ...inputs, username: e.target.value })
+                setInputs((prev) => ({ ...prev, username: e.target.value }))
               }
               _placeholder={{ color: "gray.500" }}
               type="text"
@@ -123,7 +129,9 @@ const UpdateProfilePage = () => {
             <Input
               placeholder={user.email}
               value={inputs.email}
-              onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
+              onChange={(e) =>
+                setInputs((prev) => ({ ...prev, email: e.target.value }))
+              }
               _placeholder={{ color: "gray.500" }}
               type="email"
             />
@@ -133,23 +141,44 @@ const UpdateProfilePage = () => {
             <Textarea
               placeholder={user.bio}
               value={inputs.bio}
-              onChange={(e) => setInputs({ ...inputs, bio: e.target.value })}
+              onChange={(e) =>
+                setInputs((prev) => ({ ...prev, bio: e.target.value }))
+              }
               _placeholder={{ color: "gray.500" }}
               type="text"
             />
           </FormControl>
-          <FormControl>
+          <FormControl isRequired>
             <FormLabel>Password</FormLabel>
-            <Input
-              placeholder="password"
-              value={inputs.password}
-              onChange={(e) =>
-                setInputs({ ...inputs, password: e.target.value })
-              }
-              _placeholder={{ color: "gray.500" }}
-              type="password"
-            />
+            <InputGroup>
+              <Input
+                placeholder="password"
+                value={inputs.password}
+                type={showPassword ? "text" : "password"}
+                onChange={(e) =>
+                  setInputs((prev) => ({ ...prev, password: e.target.value }))
+                }
+                _placeholder={{ color: "gray.500" }}
+              />
+              <InputRightElement h={"full"}>
+                <Button
+                  p={0}
+                  m={0}
+                  variant={"ghost"}
+                  onClick={() =>
+                    setShowPassword((showPassword) => !showPassword)
+                  }
+                >
+                  {showPassword ? (
+                    <BiSolidShow size="50%" />
+                  ) : (
+                    <BiSolidHide size="50%" />
+                  )}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
           </FormControl>
+
           <Stack pt={6} spacing={6} direction={["column", "row"]}>
             <Button
               bg={"red.400"}
