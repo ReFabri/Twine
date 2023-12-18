d53de286bbfc72bf2ecce1aa33c56f2e3ecb6b93
Renato <renato.f.fabri@gmail.com>
2023-12-17 22:56:18 -0300

    Improved CreatePost component

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -1,3 +1,4 @@
+import { useState, useRef } from "react";
 import {
   Button,
   useColorModeValue,
@@ -9,11 +10,39 @@ import {
   ModalCloseButton,
   ModalBody,
   ModalFooter,
+  FormControl,
+  Textarea,
+  Text,
+  Input,
+  Flex,
+  Image,
+  CloseButton,
 } from "@chakra-ui/react";
 import { FaPlus } from "react-icons/fa6";
+import usePreviewImg from "../hooks/usePreviewImg";
+import { BsFillImageFill } from "react-icons/bs";
 
 const CreatePost = () => {
+  const MAX_CHAR = 500;
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [postText, setPostText] = useState("");
+  const imageRef = useRef();
+  const { handleImageChange, imgUrl, setImgUrl } = usePreviewImg();
+  const [remainingChar, setRemainingChar] = useState(500);
+
+  const handleTextChange = (e) => {
+    const inputText = e.target.value;
+    if (inputText.length > MAX_CHAR) {
+      const truncatedText = inputText.slice(0, MAX_CHAR);
+      setPostText(truncatedText);
+      setRemainingChar(0);
+    } else {
+      setPostText(inputText);
+      setRemainingChar(MAX_CHAR - inputText.length);
+    }
+  };
+
+  const handleCreatePost = async () => {};
 
   return (
     <>
@@ -30,15 +59,53 @@ const CreatePost = () => {
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader>Create Post</ModalHeader>
           <ModalCloseButton />
-          <ModalBody>Some text</ModalBody>
-
+          <ModalBody pb={6}>
+            <FormControl>
+              <Textarea
+                placeholder="Post content goes here.."
+                onChange={handleTextChange}
+                value={postText}
+              />
+              <Text
+                fontSize={"xs"}
+                fontWeight={"bold"}
+                textAlign={"right"}
+                m={"1"}
+                color={"gray.800"}
+              >
+                {remainingChar}/{MAX_CHAR}
+              </Text>
+              <Input
+                type="file"
+                hidden
+                ref={imageRef}
+                onChange={handleImageChange}
+              />
+              <BsFillImageFill
+                style={{ marginLeft: "5px", cursor: "pointer" }}
+                size={16}
+                onClick={() => imageRef.current.click()}
+              />
+            </FormControl>
+            {imgUrl && (
+              <Flex mt={5} w={"full"} position={"relative"}>
+                <Image src={imgUrl} alt="Selected image" />
+                <CloseButton
+                  onClick={() => setImgUrl("")}
+                  bg={"gray.800"}
+                  position={"absolute"}
+                  top={2}
+                  right={2}
+                />
+              </Flex>
+            )}
+          </ModalBody>
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
+            <Button colorScheme="blue" mr={3} onClick={handleCreatePost}>
+              Post
             </Button>
-            <Button variant="ghost">Secondary Action</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
