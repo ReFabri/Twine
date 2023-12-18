76437dfbb63802587a6331101b39aa58ff915e2c
Renato <renato.f.fabri@gmail.com>
2023-12-18 15:23:12 -0300

    Add home and user posts links to Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,22 @@
-import { Flex, useColorMode } from "@chakra-ui/react";
-import { Image } from "@chakra-ui/react";
+import { Flex, useColorMode, Image, Link } from "@chakra-ui/react";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/userAtom";
+import { Link as RouterLink } from "react-router-dom";
+import { AiFillHome } from "react-icons/ai";
+import { RxAvatar } from "react-icons/rx";
 
 const Header = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const user = useRecoilValue(userAtom);
+
   return (
-    <Flex justifyContent={"center"} mt={6} mb="12">
+    <Flex justifyContent={user ? "space-between" : "center"} mt={6} mb="12">
+      {user && (
+        <Link as={RouterLink} to="/">
+          <AiFillHome size={24} />
+        </Link>
+      )}
+
       <Image
         cursor={"pointer"}
         alt="logo"
@@ -12,6 +24,12 @@ const Header = () => {
         src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
         onClick={toggleColorMode}
       />
+
+      {user && (
+        <Link as={RouterLink} to={`/${user.username}`}>
+          <RxAvatar size={24} />
+        </Link>
+      )}
     </Flex>
   );
 };
