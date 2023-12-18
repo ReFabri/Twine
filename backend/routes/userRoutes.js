6bdc51766046a775aaa6df9301025cd7b57c7e54
Renato <renato.f.fabri@gmail.com>
2023-12-18 16:35:18 -0300

    Add getSuggestedUsers to backend user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,7 @@ import {
   logoutUser,
   signupUser,
   updateUser,
+  getSuggestedUsers,
 } from "../controllers/userController.js";
 import protectRoute from "../middlewares/protectRoute.js";
 
@@ -14,6 +15,7 @@ const router = express.Router();
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.get("/logout", logoutUser);
+router.get("/suggested", protectRoute, getSuggestedUsers);
 router.get("/profile/:username", getUserProfile);
 router.get("/follow/:id", protectRoute, followUser);
 router.put("/update/:id", protectRoute, updateUser);
