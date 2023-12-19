e421f2953470fd62be0a77cced09cebfe5cb10d6
Renato <renato.f.fabri@gmail.com>
2023-12-19 13:35:44 -0300

    Add Homepage body and state

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,65 @@
-import { Button, Flex } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Box, Flex, Spinner } from "@chakra-ui/react";
+import { useRecoilState } from "recoil";
+import Post from "../components/Post";
+import postsAtom from "../atoms/postsAtom";
+import useShowToast from "../hooks/useShowToast";
+import SuggestedUsers from "../components/SuggestedUsers";
 
 const HomePage = () => {
+  const [posts, setPosts] = useRecoilState(postsAtom);
+  const [loading, setLoading] = useState(true);
+  const showToast = useShowToast();
+
+  useEffect(() => {
+    const getFeedPosts = async () => {
+      setLoading(true);
+      setPosts([]);
+      try {
+        const res = await fetch("/api/posts/feed");
+        const data = await res.json();
+        if (data.error) {
+          showToast("Error", data.error, "error");
+          return;
+        }
+        setPosts(data);
+      } catch (error) {
+        showToast("Error", error.message, "error");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getFeedPosts();
+  }, [showToast, setPosts]);
+
   return (
-    <Link to={"/johndoe"}>
-      <Flex w={"full"} justifyContent={"center"}>
-        <Button mx={"auto"}>Visit Profile Page</Button>
-      </Flex>
-    </Link>
+    <Flex gap="10" alignItems={"flex-start"}>
+      <Box flex={70}>
+        {!loading && posts.length === 0 && (
+          <h1>Follow some users to see the feed</h1>
+        )}
+
+        {loading && (
+          <Flex justify="center">
+            <Spinner size="xl" />
+          </Flex>
+        )}
+
+        {posts.map((post) => (
+          <Post key={post._id} post={post} postedBy={post.postedBy} />
+        ))}
+      </Box>
+      <Box
+        flex={30}
+        display={{
+          base: "none",
+          md: "block",
+        }}
+      >
+        <SuggestedUsers />
+      </Box>
+    </Flex>
   );
 };
 
