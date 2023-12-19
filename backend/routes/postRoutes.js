938f57bf05ad14a9e4a86d8d7fe9cc035ecd0639
Renato <renato.f.fabri@gmail.com>
2023-12-19 20:53:48 -0300

    Fix logic to like posts at Actions.jsx

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -17,8 +17,8 @@ router.get("/feed", protectRoute, getFeedPosts);
 router.get("/:id", getPost);
 router.get("/user/:username", getUserPosts);
 router.post("/create", protectRoute, createPost);
-router.post("/like/:id", protectRoute, likeUnlikePost);
-router.post("/reply/:id", protectRoute, replyToPost);
+router.put("/like/:id", protectRoute, likeUnlikePost);
+router.put("/reply/:id", protectRoute, replyToPost);
 router.delete("/:id", protectRoute, deletePost);
 
 export default router;
