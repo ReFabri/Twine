30b87e8aef5c727b18d9ea2d906e10248f45f037
Renato <renato.f.fabri@gmail.com>
2023-12-23 21:35:23 -0300

    Change Comment.jsx to work with correct replies data

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
 import { Flex, Avatar, Text, Divider } from "@chakra-ui/react";
-import { BsThreeDots } from "react-icons/bs";
 import PropTypes from "prop-types";
-import Actions from "./Actions";
 
-const Comment = ({ comment, createdAt, likes, username, userAvatar }) => {
-  const [liked, setLiked] = useState(false);
-
+const Comment = ({ reply, lastReply }) => {
   return (
     <>
-      <Flex gap={4} py={2} my={2} w={"full"}>
-        <Avatar src={userAvatar} size={"sm"} />
+      <Flex gap={4} py={4} my={2} w={"full"}>
+        <Avatar src={reply.userProfilePic} size={"sm"} />
         <Flex gap={1} w={"full"} flexDirection={"column"}>
           <Flex
             w={"full"}
@@ -18,33 +13,20 @@ const Comment = ({ comment, createdAt, likes, username, userAvatar }) => {
             alignItems={"center"}
           >
             <Text fontSize={"sm"} fontWeight={"bold"}>
-              {username}
+              {reply.username}
             </Text>
-            <Flex gap={2} alignItems={"center"}>
-              <Text fontSize={"sm"} color={"gray.light"}>
-                {createdAt}
-              </Text>
-              <BsThreeDots />
-            </Flex>
           </Flex>
-          <Text>{comment}</Text>
-          <Actions liked={liked} setLiked={setLiked} />
-          <Text fontSize={"sm"} color={"gray.light"}>
-            {likes + (liked ? 1 : 0)} likes
-          </Text>
+          <Text py={2}>{reply.text}</Text>
         </Flex>
       </Flex>
-      <Divider />
+      {!lastReply && <Divider />}
     </>
   );
 };
 
 Comment.propTypes = {
-  comment: PropTypes.string,
-  createdAt: PropTypes.string,
-  likes: PropTypes.number,
-  username: PropTypes.string,
-  userAvatar: PropTypes.string,
+  reply: PropTypes.object,
+  lastReply: PropTypes.bool,
 };
 
 export default Comment;
