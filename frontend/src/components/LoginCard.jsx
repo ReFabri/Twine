cdf61e0591e1e3f008400e0e1ca6fc876412ccd4
Renato <renato.f.fabri@gmail.com>
2023-12-29 14:38:21 -0300

    Add and Remove background assets

--- frontend/src/components/LoginCard.jsx
+++ frontend/src/components/LoginCard.jsx
@@ -69,95 +69,110 @@ const LoginCard = () => {
   };
 
   return (
-    <Flex align={"center"} justify={"center"}>
-      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-        <Stack align={"center"}>
-          <Heading fontSize={"4xl"} textAlign={"center"}>
-            Login
-          </Heading>
-        </Stack>
-        <Box
-          rounded={"lg"}
-          bg={useColorModeValue("white", "gray.dark")}
-          boxShadow={"lg"}
-          p={8}
-          w={{ base: "full", sm: "400px" }}
-        >
-          <Stack spacing={4}>
-            <FormControl isRequired>
-              <FormLabel>Username</FormLabel>
-              <Input
-                type="text"
-                onChange={(e) =>
-                  setInputs((prev) => ({
-                    ...prev,
-                    username: e.target.value,
-                  }))
-                }
-                value={inputs.username}
-              />
-            </FormControl>
-            <FormControl isRequired>
-              <FormLabel>Password</FormLabel>
-              <InputGroup>
+    <>
+      <Box
+        position={"absolute"}
+        top={"-20px"}
+        left={0}
+        w={"100%"}
+        h={"99dvh"}
+        backgroundImage={"twineLogin.png"}
+        backgroundRepeat={"no-repeat"}
+        backgroundSize={"cover"}
+        backgroundPosition={"center center"}
+        opacity={"30%"}
+        zIndex={-1}
+      ></Box>
+      <Flex align={"center"} justify={"center"}>
+        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+          <Stack align={"center"}>
+            <Heading fontSize={"4xl"} textAlign={"center"}>
+              Login
+            </Heading>
+          </Stack>
+          <Box
+            rounded={"lg"}
+            bg={useColorModeValue("white", "gray.dark")}
+            boxShadow={"lg"}
+            p={8}
+            w={{ base: "full", sm: "400px" }}
+          >
+            <Stack spacing={4}>
+              <FormControl isRequired>
+                <FormLabel>Username</FormLabel>
                 <Input
-                  type={showPassword ? "text" : "password"}
+                  type="text"
                   onChange={(e) =>
                     setInputs((prev) => ({
                       ...prev,
-                      password: e.target.value,
+                      username: e.target.value,
                     }))
                   }
-                  value={inputs.password}
+                  value={inputs.username}
                 />
-                <InputRightElement h={"full"}>
-                  <Button
-                    p={0}
-                    m={0}
-                    variant={"ghost"}
-                    onClick={() =>
-                      setShowPassword((showPassword) => !showPassword)
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                  <Input
+                    type={showPassword ? "text" : "password"}
+                    onChange={(e) =>
+                      setInputs((prev) => ({
+                        ...prev,
+                        password: e.target.value,
+                      }))
                     }
-                  >
-                    {showPassword ? (
-                      <BiSolidShow size="50%" />
-                    ) : (
-                      <BiSolidHide size="50%" />
-                    )}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-            </FormControl>
-            <Stack spacing={10} pt={2}>
-              <Button
-                loadingText="Logging in"
-                size="lg"
-                bg={useColorModeValue("gray.600", "gray.700")}
-                color={"white"}
-                _hover={{
-                  bg: useColorModeValue("gray.700", "gray.800"),
-                }}
-                onClick={handleLogin}
-                isLoading={loading}
-              >
-                Login
-              </Button>
-            </Stack>
-            <Stack pt={6}>
-              <Text align={"center"}>
-                Don&apos;t have an account?{" "}
-                <Link
-                  color={"blue.400"}
-                  onClick={() => setAuthScreen("signup")}
+                    value={inputs.password}
+                  />
+                  <InputRightElement h={"full"}>
+                    <Button
+                      p={0}
+                      m={0}
+                      variant={"ghost"}
+                      onClick={() =>
+                        setShowPassword((showPassword) => !showPassword)
+                      }
+                    >
+                      {showPassword ? (
+                        <BiSolidShow size="50%" />
+                      ) : (
+                        <BiSolidHide size="50%" />
+                      )}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+              <Stack spacing={10} pt={2}>
+                <Button
+                  loadingText="Logging in"
+                  size="lg"
+                  bg={useColorModeValue("gray.600", "gray.700")}
+                  color={"white"}
+                  _hover={{
+                    bg: useColorModeValue("gray.700", "gray.800"),
+                  }}
+                  onClick={handleLogin}
+                  isLoading={loading}
                 >
-                  Sign up
-                </Link>
-              </Text>
+                  Login
+                </Button>
+              </Stack>
+              <Stack pt={6}>
+                <Text align={"center"}>
+                  Don&apos;t have an account?{" "}
+                  <Link
+                    color={"blue.400"}
+                    onClick={() => setAuthScreen("signup")}
+                  >
+                    Sign up
+                  </Link>
+                </Text>
+              </Stack>
             </Stack>
-          </Stack>
-        </Box>
-      </Stack>
-    </Flex>
+          </Box>
+        </Stack>
+      </Flex>
+    </>
   );
 };
 
--- frontend/src/components/SignupCard.jsx
+++ frontend/src/components/SignupCard.jsx
@@ -77,114 +77,136 @@ const SignupCard = () => {
   };
 
   return (
-    <Flex align={"center"} justify={"center"}>
-      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-        <Stack align={"center"}>
-          <Heading fontSize={"4xl"} textAlign={"center"}>
-            Sign up
-          </Heading>
-        </Stack>
-        <Box
-          rounded={"lg"}
-          bg={useColorModeValue("white", "gray.dark")}
-          boxShadow={"lg"}
-          p={8}
-        >
-          <Stack spacing={4}>
-            <HStack>
-              <Box>
-                <FormControl isRequired>
-                  <FormLabel>Full name</FormLabel>
+    <>
+      <Box
+        position={"absolute"}
+        top={"-20px"}
+        left={0}
+        w={"100%"}
+        h={"99dvh"}
+        backgroundImage={"twineSignup.png"}
+        backgroundRepeat={"no-repeat"}
+        backgroundSize={"cover"}
+        backgroundPosition={"center center"}
+        opacity={"30%"}
+        zIndex={-1}
+      ></Box>
+
+      <Flex align={"center"} justify={"center"}>
+        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+          <Stack align={"center"}>
+            <Heading fontSize={"4xl"} textAlign={"center"}>
+              Sign up
+            </Heading>
+          </Stack>
+          <Box
+            rounded={"lg"}
+            bg={useColorModeValue("white", "gray.dark")}
+            boxShadow={"lg"}
+            p={8}
+          >
+            <Stack spacing={4}>
+              <HStack>
+                <Box>
+                  <FormControl isRequired>
+                    <FormLabel>Full name</FormLabel>
+                    <Input
+                      type="text"
+                      onChange={(e) =>
+                        setInputs((prev) => ({ ...prev, name: e.target.value }))
+                      }
+                      value={inputs.name}
+                    />
+                  </FormControl>
+                </Box>
+                <Box>
+                  <FormControl isRequired>
+                    <FormLabel>Username</FormLabel>
+                    <Input
+                      type="text"
+                      onChange={(e) =>
+                        setInputs((prev) => ({
+                          ...prev,
+                          username: e.target.value,
+                        }))
+                      }
+                      value={inputs.username}
+                    />
+                  </FormControl>
+                </Box>
+              </HStack>
+              <FormControl isRequired>
+                <FormLabel>Email address</FormLabel>
+                <Input
+                  type="email"
+                  onChange={(e) =>
+                    setInputs((prev) => ({ ...prev, email: e.target.value }))
+                  }
+                  value={inputs.email}
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
                   <Input
-                    type="text"
-                    onChange={(e) =>
-                      setInputs((prev) => ({ ...prev, name: e.target.value }))
-                    }
-                    value={inputs.name}
-                  />
-                </FormControl>
-              </Box>
-              <Box>
-                <FormControl isRequired>
-                  <FormLabel>Username</FormLabel>
-                  <Input
-                    type="text"
+                    type={showPassword ? "text" : "password"}
                     onChange={(e) =>
                       setInputs((prev) => ({
                         ...prev,
-                        username: e.target.value,
+                        password: e.target.value,
                       }))
                     }
-                    value={inputs.username}
+                    value={inputs.password}
                   />
-                </FormControl>
-              </Box>
-            </HStack>
-            <FormControl isRequired>
-              <FormLabel>Email address</FormLabel>
-              <Input
-                type="email"
-                onChange={(e) =>
-                  setInputs((prev) => ({ ...prev, email: e.target.value }))
-                }
-                value={inputs.email}
-              />
-            </FormControl>
-            <FormControl isRequired>
-              <FormLabel>Password</FormLabel>
-              <InputGroup>
-                <Input
-                  type={showPassword ? "text" : "password"}
-                  onChange={(e) =>
-                    setInputs((prev) => ({ ...prev, password: e.target.value }))
-                  }
-                  value={inputs.password}
-                />
-                <InputRightElement h={"full"}>
-                  <Button
-                    p={0}
-                    m={0}
-                    variant={"ghost"}
-                    onClick={() =>
-                      setShowPassword((showPassword) => !showPassword)
-                    }
+                  <InputRightElement h={"full"}>
+                    <Button
+                      p={0}
+                      m={0}
+                      variant={"ghost"}
+                      onClick={() =>
+                        setShowPassword((showPassword) => !showPassword)
+                      }
+                    >
+                      {showPassword ? (
+                        <BiSolidShow size="50%" />
+                      ) : (
+                        <BiSolidHide size="50%" />
+                      )}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+              <Stack spacing={10} pt={2}>
+                <Button
+                  loadingText="Submitting"
+                  size="lg"
+                  bg={useColorModeValue("gray.600", "gray.700")}
+                  color={"white"}
+                  _hover={{
+                    bg: useColorModeValue("gray.700", "gray.800"),
+                  }}
+                  onClick={handleSignup}
+                  isLoading={loading}
+                >
+                  Sign up
+                </Button>
+              </Stack>
+              <Stack pt={6}>
+                <Text align={"center"}>
+                  Already a user?{" "}
+                  <Link
+                    color={"blue.400"}
+                    onClick={() => setAuthScreen("login")}
                   >
-                    {showPassword ? (
-                      <BiSolidShow size="50%" />
-                    ) : (
-                      <BiSolidHide size="50%" />
-                    )}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-            </FormControl>
-            <Stack spacing={10} pt={2}>
-              <Button
-                loadingText="Submitting"
-                size="lg"
-                bg={useColorModeValue("gray.600", "gray.700")}
-                color={"white"}
-                _hover={{
-                  bg: useColorModeValue("gray.700", "gray.800"),
-                }}
-                onClick={handleSignup}
-                isLoading={loading}
-              >
-                Sign up
-              </Button>
+                    Login
+                  </Link>
+                </Text>
+              </Stack>
             </Stack>
-            <Stack pt={6}>
-              <Text align={"center"}>
-                Already a user?{" "}
-                <Link color={"blue.400"} onClick={() => setAuthScreen("login")}>
-                  Login
-                </Link>
-              </Text>
-            </Stack>
-          </Stack>
-        </Box>
-      </Stack>
-    </Flex>
+          </Box>
+        </Stack>
+      </Flex>
+    </>
   );
 };
 
